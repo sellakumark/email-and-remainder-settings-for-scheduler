c05da174e13e80fa8e00309f5b39172ea65309a6
Sellakumar <sellakumar.krishnamoorthi@syncfusion.com>
2020-11-02 12:07:10 +0530

    email and remainder settings for scheduler project

--- EmailScheduler/EmailServices/EmailService.cs
+++ EmailScheduler/EmailServices/EmailService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EmailScheduler.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
@@ -7,7 +8,7 @@ using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EmailScheduler.Data
+namespace EmailScheduler.EmailServices
 {
     public class EmailService : IEmailService
     {
@@ -19,13 +20,13 @@ namespace EmailScheduler.Data
             _mailConfig = mailConfig;
         }
 
-        public async Task<string> SendEmailAsync(string ToEmailName, string Subject, string HTMLBody)
+        public async Task<string> SendEmailAsync(string ToEmailName, string Subject, EventModel Data)
         {
-            return await SendEmailAsync(new List<string>() { ToEmailName }, Subject, HTMLBody);
+            return await SendEmailAsync(new List<string>() { ToEmailName }, Subject, Data);
 
         }
 
-        public async Task<string> SendEmailAsync(List<string> ToEmailName, string Subject, string HTMLBody)
+        public async Task<string> SendEmailAsync(List<string> ToEmailName, string Subject, EventModel Data)
         {
             _mailResponse = string.Empty;
 
@@ -40,19 +41,21 @@ namespace EmailScheduler.Data
                     _mailResponse = (e.Error != null || e.Cancelled != false) ? "failure" : "success";
                 });
 
-                MailMessage message = new MailMessage();
-                message.From = new MailAddress(_mailConfig.Username, _mailConfig.DisplayName);
+                MailMessage message = new MailMessage
+                {
+                    From = new MailAddress(_mailConfig.Username, _mailConfig.DisplayName),
+                    Subject = Subject,
+                    SubjectEncoding = Encoding.UTF8,
+                    BodyEncoding = Encoding.UTF8,
+                    HeadersEncoding = Encoding.UTF8,
+                    IsBodyHtml = true,
+                    Body = GetEmailContent(Subject, Data),
+                    Priority = MailPriority.High
+                };
                 foreach (string EmailName in ToEmailName)
                 {
                     message.To.Add(new MailAddress(EmailName));
                 }
-                message.Subject = Subject;
-                message.SubjectEncoding = Encoding.UTF8;
-                message.BodyEncoding = Encoding.UTF8;
-                message.HeadersEncoding = Encoding.UTF8;
-                message.IsBodyHtml = true;
-                message.Body = HTMLBody;
-                message.Priority = MailPriority.High;
 
                 await smtpClient.SendMailAsync(message);
             }
@@ -65,7 +68,7 @@ namespace EmailScheduler.Data
             return new EmailAddressAttribute().IsValid(EmailName);
         }
 
-        public string GetEmailContent()
+        private string GetEmailContent(string Title, EventModel Data)
         {
             string HTMLBody = "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\r\n" +
                 "<html lang=\"en\" xml:lang=\"en\" xmlns=\"http://www.w3.org/1999/xhtml\">\r\n\r\n" +
@@ -81,7 +84,7 @@ namespace EmailScheduler.Data
                 "<table cellspacing=\"0\" cellpadding=\"0\" width=\"100%\">\r\n" +
                 "<tbody>\r\n" +
                 "<tr>\r\n" +
-                "<td style=\"padding:0;\">\r\n<b style=\"font-size: 20px;color: #ffffff;\">###EVENTTYPE###</b>\r\n</td>\r\n" +
+                "<td style=\"padding:0;\">\r\n<b style=\"font-size: 20px;color: #ffffff;\">" + Title + "</b>\r\n</td>\r\n" +
                 "</tr>\r\n" +
                 "</tbody>\r\n" +
                 "</table>\r\n" +
@@ -102,23 +105,23 @@ namespace EmailScheduler.Data
                 "<tbody>\r\n" +
                 "<tr>\r\n" +
                 "<td style=\"background-color: #efefef; border: 2px solid #ffffff; border-collapse: collapse; padding: 10px; vertical-align: top;\"><b>Subject</b></td>\r\n" +
-                "<td style=\"background-color: #efefef; border: 2px solid #ffffff; border-collapse: collapse; padding: 10px; vertical-align: top;\">###SUBJECT###</td>\r\n" +
+                "<td style=\"background-color: #efefef; border: 2px solid #ffffff; border-collapse: collapse; padding: 10px; vertical-align: top;\">" + Data.Subject ?? "(No Title)" + "</td>\r\n" +
                 "</tr>\r\n" +
                 "<tr>\r\n" +
                 "<td style=\"background-color: #efefef; border: 2px solid #ffffff; border-collapse: collapse; padding: 10px; vertical-align: top;\"><b>Start Time</b></td>\r\n" +
-                "<td style=\"background-color: #efefef; border: 2px solid #ffffff; border-collapse: collapse; padding: 10px; vertical-align: top;\">###STARTTIME###</td>\r\n" +
+                "<td style=\"background-color: #efefef; border: 2px solid #ffffff; border-collapse: collapse; padding: 10px; vertical-align: top;\">" + Data.StartTime.ToString() + "</td>\r\n" +
                 "</tr>\r\n" +
                 "<tr>\r\n" +
                 "<td style=\"background-color: #efefef; border: 2px solid #ffffff; border-collapse: collapse; padding: 10px; vertical-align: top;\"><b>End Time</b></td>\r\n" +
-                "<td style=\"background-color: #efefef; border: 2px solid #ffffff; border-collapse: collapse; padding: 10px; vertical-align: top;\">###ENDTIME###</td>\r\n" +
+                "<td style=\"background-color: #efefef; border: 2px solid #ffffff; border-collapse: collapse; padding: 10px; vertical-align: top;\">" + Data.EndTime.ToString() + "</td>\r\n" +
                 "</tr>\r\n" +
                 "<tr>\r\n" +
                 "<td style=\"background-color: #efefef; border: 2px solid #ffffff; border-collapse: collapse; padding: 10px; vertical-align: top;\"><b>Location</b></td>\r\n" +
-                "<td style=\"background-color: #efefef; border: 2px solid #ffffff; border-collapse: collapse; padding: 10px; vertical-align: top;\">###LOCATION###</td>\r\n" +
+                "<td style=\"background-color: #efefef; border: 2px solid #ffffff; border-collapse: collapse; padding: 10px; vertical-align: top;\">" + Data.Location ?? "NA" + "</td>\r\n" +
                 "</tr>\r\n" +
                 "<tr>\r\n" +
                 "<td style=\"background-color: #efefef; border: 2px solid #ffffff; border-collapse: collapse; padding: 10px; vertical-align: top;\"><b>Description</b></td>\r\n" +
-                "<td style=\"background-color: #efefef; border: 2px solid #ffffff; border-collapse: collapse; padding: 10px; vertical-align: top;\">###DESCRIPTION###</td>\r\n" +
+                "<td style=\"background-color: #efefef; border: 2px solid #ffffff; border-collapse: collapse; padding: 10px; vertical-align: top;\">" + Data.Description ?? "NA" + "</td>\r\n" +
                 "</tr>\r\n" +
                 "</tbody>\r\n" +
                 "</table>\r\n" +
@@ -130,7 +133,7 @@ namespace EmailScheduler.Data
                 "</tbody>\r\n" +
                 "<tfoot>\r\n" +
                 "<tr class=\"##Notification##\" bgcolor=\"#6264a7\" height=\"40\" style=\"color: white;\">\r\n" +
-                "<td style=\"padding: 0 30px;\">Copyright © "+ DateTime.Now.Year.ToString() + " Syncfusion Scheduler</td>\r\n" +
+                "<td style=\"padding: 0 30px;\">Copyright © " + DateTime.Now.Year.ToString() + " Syncfusion Scheduler</td>\r\n" +
                 "</tr>\r\n" +
                 "</tfoot>\r\n" +
                 "</table>\r\n" +
@@ -138,23 +141,7 @@ namespace EmailScheduler.Data
                 "</html>";
             return HTMLBody;
         }
-    }
 
-    public interface IEmailService
-    {
-        Task<string> SendEmailAsync(string ToEmailName, string Subject, string HTMLBody);
-        Task<string> SendEmailAsync(List<string> ToEmailNames, string Subject, string HTMLBody);
-        bool IsValidEmail(string EmailName);
-        string GetEmailContent();
-    }
-
-    public class EmailSettings
-    {
-        public string Username { get; set; }
-        public string Password { get; set; }
-        public string DisplayName { get; set; }
-        public string Host { get; set; }
-        public int Port { get; set; }
     }
 
 }
